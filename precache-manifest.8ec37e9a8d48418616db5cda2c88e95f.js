0a44e6cc323e1c24f97ef33b85e3b5e2cd177473
argoneus <argoneuscze@gmail.com>
2020-02-17 15:03:08 +0100

    Updates

---
+++ precache-manifest.8ec37e9a8d48418616db5cda2c88e95f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b495a55df15463e58de0ec7ad4bb38e",
+    "url": "/argoneuscze/xdueventcalc/index.html"
+  },
+  {
+    "revision": "a6bb2bb88d58ce42e800",
+    "url": "/argoneuscze/xdueventcalc/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2ad467826a17dc8d7593",
+    "url": "/argoneuscze/xdueventcalc/static/js/2.8fafea9d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/argoneuscze/xdueventcalc/static/js/2.8fafea9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6bb2bb88d58ce42e800",
+    "url": "/argoneuscze/xdueventcalc/static/js/main.8f657734.chunk.js"
+  },
+  {
+    "revision": "b4c3cd38eccdf79b200b",
+    "url": "/argoneuscze/xdueventcalc/static/js/runtime-main.d5c636a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/argoneuscze/xdueventcalc/static/media/logo.5d5d9eef.svg"
+  }
+]);
